6c0a75343c226a957185cfde93c74b1d2bcaebf1
unknown <divyacharaninabha@gmail.com>
2020-10-20 21:26:30 +0530

    Added: Functionality to test incorrect file added

--- src/test/java/com/bridgelabz/censusanalyzer/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyzer/StateCensusAnalyserTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class StateCensusAnalyserTest {
 
 	private static String FILE_PATH = "C:\\Users\\DELL\\eclipse-workspace\\CensusAnalyzer\\src\\main\\resources\\IndianStateCensusData.csv";
+	private static String WRONG_FILE_PATH = "C:\\Users\\DELL\\CensusAnalyzer\\src\\main\\resources\\IndianStateCensusData.csv";
 
-	//To test number of entries
+	// To test number of entries
 	@Test
 	public void givenCsvDataWhenReadShouldMatchNoOfEntries() {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
@@ -19,6 +20,17 @@ public class StateCensusAnalyserTest {
 			e.printStackTrace();
 		}
 		assertEquals(29, noOfEntires);
+	}
+
+	// To test wrong file
+	@Test
+	public void givenWrongFileShouldThrowException() {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		try {
+			stateCensusAnalyser.readData(WRONG_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_EXCEPTION, e.type);
+		}
 	}
 
 }
