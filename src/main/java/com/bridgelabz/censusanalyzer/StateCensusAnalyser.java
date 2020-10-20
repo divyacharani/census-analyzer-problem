d45ba77044b79577b91795f98c5c12693c9c0f41
unknown <divyacharaninabha@gmail.com>
2020-10-20 20:08:55 +0530

    Added: Welcome message added

---
+++ src/main/java/com/bridgelabz/censusanalyzer/StateCensusAnalyser.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.censusanalyzer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StateCensusAnalyser {
+
+	private static final Logger LOG = LogManager.getLogger(StateCensusAnalyser.class);
+
+	public static void main(String[] args) {
+
+		// Welcome Message
+		LOG.info("Welcome to Indian State Census Analyser program");
+
+	}
+}
